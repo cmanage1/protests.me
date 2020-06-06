a1b95815fb1df13d9f4e82d041b47fad48925c4b
cmanage1 <chethinlm@gmail.com>
2020-06-05 20:19:50 -0400

    firebase init stuff

---
+++ main.js
@@ -0,0 +1,8 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyCj7mkuwUwj9N8o1LYjOy5q5_Sii52ven4',
+    authDomain: 'protests-dot-me.firebaseapp.com',
+    projectId: 'protestsdotme'
+});
+
+var db = firebase.firestore();
