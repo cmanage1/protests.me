ad0ccc70ecaa4bf993390343f8436c8af194c0b6
cmanage1 <chethinlm@gmail.com>
2020-06-06 12:10:20 -0400

    working data writing + added firebaseUI testing

--- server/main.js
+++ server/main.js
@@ -1,10 +1,27 @@
-const title = document.getElementById('title');
-const date = document.getElementById('date');
-const location = document.getElementById('date');
-const description = document.getElementById('date');
-var participants = document.getElementById('participants');
+// const title = document.getElementById('title');
+// const date = document.getElementById('date');
+// const location = document.getElementById('date');
+// const description = document.getElementById('date');
+// var participants = document.getElementById('participants');
 //const data = {title, date, location, description, participants}
-private.db.collection("protests").add({
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+const title = "Some big Protest";
+const description = "Example Protest Description 21312414";
+const location = "LA";
+const participants = 543534;
+const date = "June 20";
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyCj7mkuwUwj9N8o1LYjOy5q5_Sii52ven4',
+    authDomain: 'protests-dot-me.firebaseapp.com',
+    projectId: 'protestsdotme'
+});
+
+var db = firebase.firestore();
+db.collection("protests").add({
     date : date,
     description : description,
     location : location,
@@ -17,23 +34,8 @@ private.db.collection("protests").add({
 });
 
 
-var docRef = db.collection("protests").doc(index.html);
+var docRef = db.collection("protests").doc('xIuT8WaGHoAwI21UFIUL');
 
-<<<<<<< HEAD
-private.db.collection("protests").add({
-    date: date,
-    description: description,
-    location: location,
-    numParticipants: participants,
-    title: title
-}).then(function (docRef) {
-    console.log("Document written with ID: ", docRef.id);
-}).catch(function (error) {
-    console.error("Error adding document: ", error);
-});
-
-
-var docRef = db.collection("protests").doc(index.html);
 
 // Valid options for source are 'server', 'cache', or
 // 'default'. See https://firebase.google.com/docs/reference/js/firebase.firestore.GetOptions
@@ -50,20 +52,3 @@ docRef.get(getOptions).then(function (doc) {
 }).catch(function (error) {
     console.log("Error getting cached document:", error);
 });
-=======
-// Valid options for source are 'server', 'cache', or
-// 'default'. See https://firebase.google.com/docs/reference/js/firebase.firestore.GetOptions
-// for more information.
-var getOptions = {
-    source: 'cache'
-};
-
-// Get a document, forcing the SDK to fetch from the offline cache.
-docRef.get(getOptions).then(function(doc) {
-    // Document was found in the cache. If no cached document exists,
-    // an error will be returned to the 'catch' block below.
-    console.log("Cached document data:", doc.data());
-}).catch(function(error) {
-    console.log("Error getting cached document:", error);
-});
->>>>>>> d6386a9a25f271862a32e45afaf1e7ccf7e7d605
